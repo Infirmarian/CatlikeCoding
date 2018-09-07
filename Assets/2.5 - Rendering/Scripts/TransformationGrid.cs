8d4efff9bc32b79b6a51d1cb6470412d8bed058f
Robert Geil <robert@rgeil.net>
2018-09-06 22:34:19 -0400

    Start Rendering 1
    - Started the Rendering Tutorial # 1

---
+++ Assets/2.5 - Rendering/Scripts/TransformationGrid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformationGrid : MonoBehaviour {
+    public Transform prefab;
+    public int resolution;
+    Transform[] grid;
+
+    private void Awake() {
+        grid = new Transform[resolution * resolution * resolution];
+        for (int i = 0, z = 0; z < resolution; z++){
+            for (int y = 0; y < resolution;y++){
+                for (int x = 0; x < resolution; i++, x++){
+                    grid[i] = CreateGridPoint(x, y, z);
+                }
+            }
+        }
+    }
+
+    private Transform CreateGridPoint(int x, int y, int z){
+        Transform point = Instantiate<Transform>(prefab);
+        point.localPosition = GetCoordinates(x, y, z);
+        point.GetComponent<MeshRenderer>().material.color = new Color(
+            (float)x / resolution, (float)y / resolution, (float)z / resolution);
+        return point;
+    }
+
+    private Vector3 GetCoordinates(int x, int y, int z){
+        return new Vector3(
+            x - (resolution - 1) * 0.5f, y - (resolution - 1) * 0.5f, z - (resolution - 1) * 0.5f);
+    }
+}
