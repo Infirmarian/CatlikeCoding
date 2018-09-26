fd7ee89307f63d38f9197b4441350f81ac4a8289
Robert Geil <robert.geil999@gmail.com>
2018-09-25 23:32:37 -0700

    Added scale and transforming to first rendering series

--- Assets/2.5 - Rendering/Scripts/TransformationGrid.cs
+++ Assets/2.5 - Rendering/Scripts/TransformationGrid.cs
@@ -7,6 +7,8 @@ public class TransformationGrid : MonoBehaviour {
     public int resolution;
     Transform[] grid;
 
+    List<Transformation> transformations;
+
     private void Awake() {
         grid = new Transform[resolution * resolution * resolution];
         for (int i = 0, z = 0; z < resolution; z++){
@@ -16,7 +18,29 @@ public class TransformationGrid : MonoBehaviour {
                 }
             }
         }
+        transformations = new List<Transformation>();
     }
+
+    private void Update(){
+
+        GetComponents<Transformation>(transformations);
+        for(int i=0, z=0; z < resolution; z++){
+            for(int y = 0; y<resolution; y++){
+                for( int x = 0; x <resolution; x++, i++){
+                    grid[i].localPosition = TransformPoint(x,y,z);
+                }
+            }
+        }
+    }
+
+    Vector3 TransformPoint(int x, int y, int z){
+        Vector3 coordinates = GetCoordinates(x,y,z);
+        for ( int i = 0; i< transformations.Count; i++){
+            coordinates = transformations[i].Apply(coordinates);
+        }
+        return coordinates;
+    }
+
 
     private Transform CreateGridPoint(int x, int y, int z){
         Transform point = Instantiate<Transform>(prefab);
@@ -28,6 +52,8 @@ public class TransformationGrid : MonoBehaviour {
 
     private Vector3 GetCoordinates(int x, int y, int z){
         return new Vector3(
-            x - (resolution - 1) * 0.5f, y - (resolution - 1) * 0.5f, z - (resolution - 1) * 0.5f);
+            x - (resolution - 1) * 0.5f, 
+            y - (resolution - 1) * 0.5f, 
+            z - (resolution - 1) * 0.5f);
     }
 }
